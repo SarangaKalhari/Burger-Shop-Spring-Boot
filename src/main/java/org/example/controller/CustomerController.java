4d931e1b59082656c3148449f09d3fc613e8ec28
SarangaKalhari <kalharisaranga9@gmail.com>
2025-11-27 18:59:47 +0530

    create an API for testing

---
+++ src/main/java/org/example/controller/CustomerController.java
@@ -0,0 +1,13 @@
+package org.example.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CustomerController {
+
+    @GetMapping
+    public String getStart(){
+        return "Welcome Our Burger Shop";
+    }
+}
