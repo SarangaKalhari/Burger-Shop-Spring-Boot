5079433dabe94fa079fb3caea322f0708bc5c766
SarangaKalhari <kalharisaranga9@gmail.com>
2025-11-27 21:04:05 +0530

    Implement Add Customer feature (Controller, Service Repository)

--- src/main/java/org/example/controller/CustomerController.java
+++ src/main/java/org/example/controller/CustomerController.java
@@ -1,8 +1,11 @@
 package org.example.controller;
 
+import org.example.model.DTO.CustomerDTO;
 import org.example.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,5 +17,10 @@ public class CustomerController {
     @GetMapping
     public String getStart(){
         return "Welcome Our Burger Shop";
+    }
+
+    @PostMapping("/add")
+    public void addCustomer(@RequestBody CustomerDTO customerDTO){
+        customerService.save(customerDTO);
     }
 }
--- src/main/java/org/example/service/CustomerService.java
+++ src/main/java/org/example/service/CustomerService.java
@@ -1,5 +1,7 @@
 package org.example.service;
 
+import org.example.model.DTO.CustomerDTO;
+import org.example.model.Entity.Customer;
 import org.example.repository.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,4 +11,22 @@ public class CustomerService {
 
     @Autowired
     CustomerRepository customerRepository;
+
+    public void save(CustomerDTO customerDTO) {
+
+        Customer customer = new Customer(
+                1L,
+                customerDTO.getPhoneNumber(),
+                customerDTO.getCustTitle(),
+                customerDTO.getCustName(),
+                customerDTO.getDob(),
+                customerDTO.getSalary(),
+                customerDTO.getCustAddress(),
+                customerDTO.getCity(),
+                customerDTO.getProvince(),
+                customerDTO.getPostalCode()
+        );
+
+        customerRepository.save(customer);
+    }
 }
