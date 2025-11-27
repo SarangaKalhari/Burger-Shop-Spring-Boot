11f4878c7ce0019e0754a60983d269080939ed30
SarangaKalhari <kalharisaranga9@gmail.com>
2025-11-27 19:42:12 +0530

    Add DTO class and Entity class for customer

--- src/main/java/org/example/model/DTO/CustomerDTO.java
+++ src/main/java/org/example/model/DTO/CustomerDTO.java
@@ -1,4 +1,24 @@
 package org.example.model.DTO;
 
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class CustomerDTO {
+
+    private long phoneNumber;
+    private String custTitle;
+    private String custName;
+    private LocalDate dob;
+    private BigDecimal salary;
+    private String custAddress;
+    private String city;
+    private String province;
+    private String postalCode;
 }
--- src/main/java/org/example/model/Entity/Customer.java
+++ src/main/java/org/example/model/Entity/Customer.java
@@ -1,4 +1,30 @@
 package org.example.model.Entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Entity
 public class Customer {
+
+    @Id
+    private long cust_id;
+
+    private long phone_number;
+    private String cust_title;
+    private String cust_name;
+    private LocalDate dob;
+    private BigDecimal salary;
+    private String cust_address;
+    private String city;
+    private String province;
+    private String postal_code;
 }
